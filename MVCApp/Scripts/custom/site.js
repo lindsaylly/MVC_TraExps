8d37b6528235dedad4b559061acb79554b4d376d
Lindsay <39603680+lindsaylly@users.noreply.github.com>
2021-04-23 08:38:00 -0600

    Update MVCApp

---
+++ MVCApp/Scripts/custom/site.js
@@ -0,0 +1,18 @@
+﻿function LoadProvinceList(name) {
+    $.get("/Home/GetProvinceList", { CountryName: name }, function (data) {
+        $("#Province").empty();
+        $("#Province").append("<option></option>");
+        $.each(data, function (index, row) {
+            $("#Province").append("<option value='" + row.ProvAbbr + "'>" + row.ProvName + "</option>")
+        });
+    });
+}
+
+
+function MaskPhoneNumber() 
+{
+    $("#BusinessPhone, #HomePhone").mask("(999) 999-9999");
+}
+
+
+
